3ec9a487427bdd8ae54b6a47aed31ec8fbab29ad
Cameron Alizadeh <cdalizadeh@gmail.com>
2018-01-28 15:59:52 -0500

    all images downloaded

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 import os
 import re
-import urllib
+import urllib2
+import hashlib
 
 def clean_text(gender):
     #input: 'males' or 'females'
@@ -9,15 +10,44 @@ def clean_text(gender):
     clean = []              #format is [name, url, coordinates, hash]
     for line in lines:
         line_split = re.split(r'\t+', line)
+        print line_split
         new_line = [line_split[0], line_split[3], line_split[4], line_split[5]]
         clean.append(new_line)
     return clean
 
+def download_images(clean):
+    for image_line in clean:
+        name = image_line[0]
+        url = image_line[1]
+        coordinates = image_line[2]
+        hashcode = image_line[3].rstrip("\n\r") #rstrip required to remove newline symbol
+
+        try:
+            request = urllib2.urlopen(url, timeout=10)
+        except:
+            print("error opening url")
+            continue
+        
+        try:
+            filedata = request.read()
+        except:
+            print("error reading file")
+            continue
+
+        if hashlib.sha256(filedata).hexdigest() != hashcode:
+            print("hashes did not match")
+            continue
+
+        filename = name + "-" + coordinates + "-" + hashcode
+        imgfile = open("./data/raw/" + filename, "wb")
+        try:
+            imgfile.write(filedata)
+        except:
+            print("error writing file data")
+            continue
+        
+        print "success"
+
 if __name__ == "__main__":
-    #try:
-    #    print urllib.URLopener().retrieve(urls[1], "./test.jpg")
-    #except:
-    #    print "failure"
-    #print re.split(r'\t+', males_list[0])
-    clean = clean_text("males")
-    print clean[0]
+    clean = clean_text("females")
+    download_images(clean)
